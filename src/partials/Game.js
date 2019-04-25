56f82763f6153b63abcbc82a2e930db579595868
ronald <r.gaskin@live.ca>
2019-04-25 14:01:33 -0700

    working-w-board

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,5 +1,5 @@
 import { SVG_NS } from "../settings";
-
+import board from './partials/board';
 
 
 export default class Game {
@@ -9,17 +9,21 @@ export default class Game {
     this.height = height;
 
     this.gameElement = document.getElementById(this.element);
+
+    this.board = new board(this.width, this.height);
     // Other code goes here...
   }
 
   render() {
     // More code goes here....
+    
+    this.gameElement.innerHTML = '';
     let svg = document.createElementNS(SVG_NS, "svg");
-svg.setAttributeNS(null, "width", this.width);
-svg.setAttributeNS(null, "height", this.height);
-svg.setAttributeNS(null, "viewBox", `0 0 ${this.width} ${this.height}`);
-this.gameElement.appendChild(svg);
-
-this.gameElement.innerHTML = '';
+    svg.setAttributeNS(null, "width", this.width);
+    svg.setAttributeNS(null, "height", this.height);
+    svg.setAttributeNS(null, "viewBox", `0 0 ${this.width} ${this.height}`);
+    this.gameElement.appendChild(svg);
+    
+    this.board.render(svg);
   }
 }
